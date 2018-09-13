c2499b6e89b2b49e7e2543b85cf48afc4603c13f
Uthaeus <romanlavery@gmail.com>
2018-09-13 10:19:01 -0600

    Created Book component and Library container

---
+++ src/containers/Library/Library.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Book from '../../components/Book';
+
+class Library extends Component {
+    state = {};
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
